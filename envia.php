ff4a969fc37c39c0cb96cadfb12eebf99a48dc69
prog.danubio <prog.danubio@hotmail.com>
2018-08-17 13:20:26 -0300

    Form test

--- envia.php
+++ envia.php
@@ -5,27 +5,27 @@ include_once('conexao.php');
 
 $assunto = utf8_decode (strip_tags(trim($_POST['assunto'])));
 $mensagem = utf8_decode (strip_tags(trim($_POST['mensagem'])));
+
 require_once('PHPMailer/class.phpmailer.php');
 
 $Email = new PHPMailer();
 $Email->SetLanguage("br");
 $Email->IsSMTP(); // Habilita o SMTP
 $Email->SMTPAuth = true; //Ativa e-mail autenticado
-$Email->Host = 'bol.com.br'; // Servidor de envio
+$Email->Host = 'smtp.gmail.com'; // Servidor de envio
 $Email->Port = '587'; // Porta de envio
-$Email->Username = 'jodanubio@bol.com.br'; //e-mail que será autenticado
-$Email->Password = '555666'; // senha do email
+$Email->Username = '@gmail.com'; //e-mail que será autenticado
+$Email->Password = 'senhar'; // senha do servidor smtp
 
 $Email->IsHTML(true);
 
-$Email->From = 'jodanubio@bol.com.br';
-
-$Email->FromName = utf8_decode($assunto);
+$Email->From = '@gmail.com'; //email de destino
+$Email->FromName = "Teste";
 
 $Email->AddReplyTo($assunto, $mensagem);
-$Email->AddAddress("danubio.bwm@gmail.com.br");
+$Email->AddAddress("@gmail.com", 'Teste');
 
-$Email->Subject = "(bol.com.br)";
+$Email->Subject = "(Contato do site)";
 
 $Email->Body .= "<br /><br />
 <strong>Assunto:</strong> $assunto<br /><br />
@@ -39,9 +39,9 @@ if(!$Email->Send()){
     
 }
 $assunto = mysqli_real_escape_string($conn, $_POST['assunto']);
-	$mensagem = mysqli_real_escape_string($conn, $_POST['mensagem']);
-	
-	
-	$result_msg_contato = "INSERT INTO mensagens_contatos(assunto, mensagem, created) VALUES ('$assunto', '$mensagem', NOW())";
-	$resultado_msg_contato= mysqli_query($conn, $result_msg_contato)
+$mensagem = mysqli_real_escape_string($conn, $_POST['mensagem']);
+
+
+$result_msg_contato = "INSERT INTO mensagens_contatos(assunto, mensagem, created) VALUES ('$assunto', '$mensagem', NOW())";
+$resultado_msg_contato= mysqli_query($conn, $result_msg_contato)
 ?>
